2d2ddefd4e281bf65b3e6cae01882677df2dcf5f
Salman Saputra <salmansaputra76@gmail.com>
2026-01-21 01:36:59 +0700

    Minor update confirmation message on facility index page

--- resources/views/facility/index.blade.php
+++ resources/views/facility/index.blade.php
@@ -35,7 +35,8 @@
                                 <label for="input-group-1" class="sr-only">Search</label>
                                 <div class="relative">
                                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                        <svg class="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        <svg class="w-4 h-4 text-body" aria-hidden="true"
+                                             xmlns="http://www.w3.org/2000/svg"
                                              width="24" height="24" fill="none" viewBox="0 0 24 24">
                                             <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
                                                   d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
@@ -91,7 +92,7 @@
                                             @csrf
                                             @method('delete')
                                             <button type="submit" class="red-btn"
-                                                    onclick="return confirm('Are you sure want to delete this room?');">
+                                                    onclick="return confirm('Are you sure want to delete this facility?');">
                                                 <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none"
                                                      stroke="currentColor"
                                                      stroke-width="2">
