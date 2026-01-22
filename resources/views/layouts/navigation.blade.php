3dc47d8d8eb7eb2765959b43a70163c3612bf576
Salman Saputra <salmansaputra76@gmail.com>
2026-01-23 00:34:45 +0700

    Add role validation for some menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,4 @@
+@php use App\Support\Constant; @endphp
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -6,7 +7,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800"/>
                     </a>
                 </div>
 
@@ -21,28 +22,34 @@
                         {{ __('Bookings') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden sm:flex space-x-8 sm:-my-px sm:ms-10">
-                    <x-nav-link :href="route('rooms.index')" :active="request()->routeIs('rooms*')">
-                        {{ __('Rooms') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden sm:flex space-x-8 sm:-my-px sm:ms-10">
-                    <x-nav-link :href="route('facilities.index')" :active="request()->routeIs('facilities*')">
-                        {{ __('Facilities') }}
-                    </x-nav-link>
-                </div>
+                @if(auth()->user()->role === Constant::ROLE_ADMIN)
+                    <div class="hidden sm:flex space-x-8 sm:-my-px sm:ms-10">
+                        <x-nav-link :href="route('rooms.index')" :active="request()->routeIs('rooms*')">
+                            {{ __('Rooms') }}
+                        </x-nav-link>
+                    </div>
+                    <div class="hidden sm:flex space-x-8 sm:-my-px sm:ms-10">
+                        <x-nav-link :href="route('facilities.index')" :active="request()->routeIs('facilities*')">
+                            {{ __('Facilities') }}
+                        </x-nav-link>
+                    </div>
+                @endif
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button
+                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                          clip-rule="evenodd"/>
                                 </svg>
                             </div>
                         </button>
@@ -58,7 +65,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                             onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -69,10 +76,14 @@
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <button @click="open = ! open"
+                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex"
+                              stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="M4 6h16M4 12h16M4 18h16"/>
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round"
+                              stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
                     </svg>
                 </button>
             </div>
@@ -88,12 +99,14 @@
             <x-responsive-nav-link :href="route('bookings.index')" :active="request()->routeIs('bookings*')">
                 {{ __('Bookings') }}
             </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('rooms.index')" :active="request()->routeIs('rooms*')">
-                {{ __('Rooms') }}
-            </x-responsive-nav-link>
-            <x-responsive-nav-link :href="route('facilities.index')" :active="request()->routeIs('facilities*')">
-                {{ __('Facilities') }}
-            </x-responsive-nav-link>
+            @if(auth()->user()->role === Constant::ROLE_ADMIN)
+                <x-responsive-nav-link :href="route('rooms.index')" :active="request()->routeIs('rooms*')">
+                    {{ __('Rooms') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('facilities.index')" :active="request()->routeIs('facilities*')">
+                    {{ __('Facilities') }}
+                </x-responsive-nav-link>
+            @endif
         </div>
 
         <!-- Responsive Settings Options -->
@@ -113,7 +126,7 @@
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                                           onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
